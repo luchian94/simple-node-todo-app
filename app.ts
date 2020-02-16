765eb4fdc7cf6d84c829bbae6db8539cb951a184
Luchian Bordiyan <luchian.bordiyan@webscience.it>
2020-02-16 16:37:45 +0100

    Added MYSQL connection with query examples

--- app.ts
+++ app.ts
@@ -1,29 +1,15 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import {Todo} from "./models/Todo.model";
+import MySQL from "./mysql";
 
 class App {
 
     public express: express.Application;
 
-    todos: Todo[];
-
     constructor() {
         this.express = express();
         this.middleware();
         this.routes();
-        this.todos = [
-            {
-                id: 1,
-                title: 'Testing',
-                description: 'Test new features'
-            },
-            {
-                id: 2,
-                title: 'Publish production',
-                description: 'Publish app in production'
-            }
-        ];
     }
 
     private middleware(): void {
@@ -39,19 +25,50 @@ class App {
 
         this.express.get("/todo", (req, res, next) => {
             console.log("url:::::::" + req.url);
-            res.json(this.todos);
+            const query = `SELECT * FROM todo`;
+            MySQL.query(query, (err:Error, results:Object[])=>{
+                if (err) {
+                    res.status(400)
+                        .send({
+                            success: false,
+                            status: res.status
+                        });
+                } else {
+                    res.status(200)
+                        .send({
+                            success: true,
+                            status: res.status,
+                            todo: results
+                        });
+                }
+            });
         });
 
         this.express.get("/todo/:todoId", (req, res, next) => {
             console.log("url:::::::" + req.url);
-            const todo = this.todos.find( t => t.id === parseInt(req.params.todoId, 10) );
-            res.json(todo);
+            const query = `SELECT * FROM todo WHERE id=${MySQL.escape(req.params.id)}`
+            MySQL.query(query, (err:Error, result:Object[]) => {
+                if(err){
+                    res.status(200)
+                        .send({
+                            success: false,
+                            status: res.status
+                        });
+                }
+                else{
+                    res.status(200)
+                        .send({
+                            success: true,
+                            status: res.status,
+                            todo: result
+                        });
+                }
+            });
         });
 
         this.express.post("/todo", (req, res, next) => {
             console.log("url:::::::" + req.url);
-            this.todos.push(req.body);
-            res.json(this.todos);
+            // TODO: query inserimento
         });
 
         this.express.use('*', (req, res, next) => {
